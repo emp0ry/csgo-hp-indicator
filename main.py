2a01fb9f45df4ab93a53a7467d7a5257368dd19e
emp0ry <64217088+emp0ry@users.noreply.github.com>
2023-07-17 15:27:51 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import telnetlib
+import time
+
+def main():
+    while True:
+        try:
+            tn = telnetlib.Telnet('127.0.0.1', 2121)
+            info_save = list()
+            while True:
+                data = tn.read_until(b"\n").decode("utf-8").replace('\n', '')
+                data2 = data
+                if 'Damage Given to' in data:
+                    data = data.replace("Damage Given to ", "").replace('"', '').split(' in')[0].split('- ')
+                    if int(data[-1]) < 100:
+                        if data2 not in info_save:
+                            info_save.append(data2)
+                            data = data[0] + '-' + data[-1] + ' hp'
+                            tn.write(f'say_team {data} \n'.encode('utf-8'))
+                            time.sleep(1)
+                        else:
+                            info_save.remove(data2)
+        except:
+            print(f"failed to connect to csgo (game not open? -netconport 2121 not set?)")
+            time.sleep(1)
+
+if __name__ == '__main__':
+    print('''
+█░█ █▀█   █ █▄░█ █▀▄ █ █▀▀ ▄▀█ ▀█▀ █▀█ █▀█
+█▀█ █▀▀   █ █░▀█ █▄▀ █ █▄▄ █▀█ ░█░ █▄█ █▀▄
+
+█▄▄ █▄█   █▀▀ █▀▄▀█ █▀█ █▀█ █▀█ █▄█
+█▄█ ░█░   ██▄ █░▀░█ █▀▀ █▄█ █▀▄ ░█░
+https://github.com/emp0ry/''')
+    main()
